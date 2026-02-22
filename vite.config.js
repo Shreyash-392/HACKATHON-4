9f916e9bb66f0754e9cb8e0fd5e20577a06a6995
Shreyash <shreyashn392@gmail.com>
2026-02-22 17:53:00 +0530

    Fix Vite base path

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Hackathon-4/',
   plugins: [react()],
-  server: {
-    port: 5173
-  }
+  base: '/',   // 👈 ADD THIS LINE
 })
